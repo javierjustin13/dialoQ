982e8bd175ce312e56d9cc9e7bcd0cfd9ab9074a
Interblues88 <delvin.setiamin.08@gmail.com>
2023-11-14 10:56:19 +0700

    making dialog card

--- resources/views/widgets/dialog-card.blade.php
+++ resources/views/widgets/dialog-card.blade.php
@@ -1,9 +1,36 @@
 @push('head')
-<link rel="stylesheet" href="{{ asset('css/widgets/dialog-card.css') }}"> {{--ini buat nyambungin home.css ke home blade nya --}}
+<link rel="stylesheet" href="{{ asset('css/widgets/dialog-card.css') }}"> 
 @endpush
 
 <div class="p-3 border rounded-4" id="midpane">
     <div class="p-3 border rounded-4" id="postcard">
-        Custom column padding
+        <div class="card-header">
+            <div class="media flex-wrap w-100 align-items-center"> <img src="image/profile-image.jpeg" class="d-block ui-w-40 rounded-circle" alt="">
+                <div class="media-body ml-3"> <a href="javascript:void(0)" data-abc="true">Tom Harry</a>
+                </div>
+                <div class="text-muted small ml-3">
+                <a href="">View</a>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            <h4>One Piece Exthibition</h4>
+            <p> For me, getting my business website made was a lot of tech wizardry things. Thankfully i get an ad on Facebook ragarding commence website. I get connected with BBB team. They made my stunning website live in just 3 days.
+                With the increase demand of online customers.
+            </p>
+        </div>
+        <div class="card-footer d-flex flex-wrap justify-content-between align-items-center px-0 pt-0 pb-3">
+            <div class="px-4 pt-3"> 
+                <a href="javascript:void(0)" class="text-muted d-inline-flex align-items-center align-middle" data-abc="true"> 
+                    <i class="fa fa-heart text-danger"></i>
+                    <span class="align-middle">445</span> 
+                </a> 
+                <span class="text-muted d-inline-flex align-items-center align-middle ml-4"> 
+                    <i class="fa fa-eye text-muted fsize-3"></i> 
+                    <span class="align-middle">14532</span> 
+                </span> 
+            </div>
+        </div>
+
     </div>
 </div>
