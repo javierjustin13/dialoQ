c0781619d7bff36785a676f06899007084c660a0
Javier Justin <jav.justin2004.jj@gmail.com>
2023-11-14 10:55:11 +0700

    fixed error in migrations

--- database/migrations/2023_11_14_032923_create_comments_table.php
+++ database/migrations/2023_11_14_032923_create_comments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('dialog_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('dialogues_id')->constrained()->cascadeOnDelete();
             $table->string('content');
             $table->timestamps();
         });
