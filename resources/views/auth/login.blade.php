0524d016efdf8931b39c5e06282c269a26f6c783
Kezia Revalina Tan <120441245+sparkel124@users.noreply.github.com>
2023-11-14 14:30:24 +0700

    login & register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.app')
 
 @push('head')
-
+<link rel="stylesheet" href="{{ asset('css/auth/login.css') }}">
 @endpush
+
+@section('title', 'Login')
+
+@section('content')
+<div class="form-login">
+    <div class="container-md-offset-3 row-md-6">
+            <form class="form-horizontal">
+                <div class="heading">
+                    <div class="login">LOGIN</div>
+                </div>
+                <div class="login-req">Email : </div>
+                <div class="form-group">
+                    <input type="email" class="form-control" id="inputemail" placeholder="example@gmail.com">
+                    {{-- <i class="fa fa-user"></i> --}}
+                </div>
+                <div class="login-req">Password :</div>
+                <div class="form-group">
+                    <input type="password" class="form-control" id="inputPassword" placeholder="more than 8 characters">
+                    {{-- <i class="fa fa-lock"></i> --}}
+                    <a href="#" class="fa fa-question-circle"></a>
+                </div>
+                <div class="submit-btn">
+                    <button type="submit" class="button">SUBMIT</button>
+                </div>
+                <div class="question">Don't have an account yet? <a href="/register">Register here</a></div>
+            </form>
+    </div>
+</div>
+@endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,45 @@
 @extends('layouts.app')
 
 @push('head')
-
+<link rel="stylesheet" href="{{ asset('css/auth/register.css') }}">
 @endpush
+
+@section('title', 'Register')
+
+@section('content')
+<div class="form-register">
+    <div class="container-md-offset-3 row-md-6">
+            <form class="form-horizontal">
+                <div class="heading">
+                    <div class="register">REGISTER</div>
+                </div>
+                <div class="login-req">Username : </div>
+                <div class="form-group">
+                    <input type="username" class="form-control" id="inputUsername" placeholder="ex. delvin88">
+                    {{-- <i class="fa fa-user"></i> --}}
+                </div>
+                <div class="login-req">Email : </div>
+                <div class="form-group">
+                    <input type="email" class="form-control" id="inputEmail" placeholder="example@gmail.com">
+                    {{-- <i class="fa fa-user"></i> --}}
+                </div>
+                <div class="login-req">Password :</div>
+                <div class="form-group">
+                    <input type="password" class="form-control" id="inputPassword" placeholder="more than 8 characters">
+                    {{-- <i class="fa fa-lock"></i> --}}
+                    {{-- <a href="#" class="fa fa-question-circle"></a> --}}
+                </div>
+                <div class="login-req">Confirm password :</div>
+                <div class="form-group">
+                    <input type="password" class="form-control" id="inputPassword" placeholder="enter your password">
+                    {{-- <i class="fa fa-lock"></i> --}}
+                    {{-- <a href="#" class="fa fa-question-circle"></a> --}}
+                </div>
+                <div class="submit-btn">
+                    <button type="submit" class="button">SUBMIT</button>
+                </div>
+                <div class="question">Have an account? <a href="/login">Login here</a></div>
+            </form>
+    </div>
+</div>
+@endsection
