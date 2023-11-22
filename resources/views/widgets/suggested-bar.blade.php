6b63a5126a5c312cea60dd58870a75efa6227fc7
Javier Justin <jav.justin2004.jj@gmail.com>
2023-11-22 13:05:22 +0700

    tweaked suggested bar

--- resources/views/widgets/suggested-bar.blade.php
+++ resources/views/widgets/suggested-bar.blade.php
@@ -11,18 +11,18 @@
         </div>
         <div class="suggestedpeople pb-3">
             <div class="suggestedname">
-                <img src="image/profile-image.jpeg" alt="">
+                <img src="../image/profile-image.jpeg" alt="">
                 <p class="pl-2">Javier Justin</p>
             </div>
-            <img src="image/add-user.png" alt="">
+            <img src="../image/add-user.png" alt="">
         </div>
         <div class="suggestedpeople">
             <div class="suggestedname">
-                <img src="image/profile-image.jpeg" alt="">
+                <img src="../image/profile-image.jpeg" alt="">
                 <p class="pl-2">Kezia revalina</p>
             </div>
-            <img src="image/add-user.png" alt="">
+            <img src="../image/add-user.png" alt="">
         </div>
-        
+
     </div>
 </div>
