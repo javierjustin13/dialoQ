83d363ec6b5a43e8e8b42b656f79b9550004ac4e
Javier Justin <jav.justin2004.jj@gmail.com>
2023-11-23 11:23:46 +0700

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,15 +30,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
-    <script>
-        function disable(button){
-            button.disabled = true;
-
-            setTimeout(function(){
-                button.disabled = false;
-            }, 5000);
-        }
-    </script>
 </body>
 
 </html>
