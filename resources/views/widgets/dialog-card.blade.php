302749116d3ad2e9ac783127016d1c8857b1a221
gladysruslim <gladysruslim22@gmail.com>
2023-11-28 11:14:53 +0700

    fixed read more button at dialog card

--- resources/views/widgets/dialog-card.blade.php
+++ resources/views/widgets/dialog-card.blade.php
@@ -35,9 +35,10 @@
         @if ($contentLength > 80)
             @php
                 $content1 = Str::substr($content, 0, 80);
-                $content2 = Str::substr($content, 81, $contentLength);
+                $content2 = Str::substr($content, 80, $contentLength);
             @endphp
-            <p>{{ $content1 }} <span class="dots">...</span>    <span class="more">{{ $content2 }}</span></p>
+            <p>{{ $content1 }}<span class="dots">...</span>
+            <span class="more">{{ $content2 }}</span></p>
             <button onclick="readMore(this)" class="read-more-button">Read more</button>
         @else
             <p>{{ $dialog->content }}</p>
