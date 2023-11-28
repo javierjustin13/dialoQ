d57e610097ed0352b556940d1c1bb4d57ecfdd66
Kezia Revalina Tan <120441245+sparkel124@users.noreply.github.com>
2023-11-28 23:19:36 +0700

    fixed register page and login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,21 +18,20 @@
             <div class="form-group"><input class="inputbox" type="email" name="email"
                     placeholder="example@gmail.com"></div>
             @error('email')
-                <span class="d-block fs-6 text-danger my-2">{{ $message }}</span>
+                <span class="error text-danger">{{ $message }}</span>
             @enderror
 
             <div class="inputtitle">Password</div>
             <div class="form-group"><input class="inputbox" type="password" name="password"
                     placeholder="more than 8 characters"></div>
             @error('password')
-                <span class="d-block fs-6 text-danger my-2">{{ $message }}</span>
+                <span class="error text-danger">{{ $message }}</span>
             @enderror
 
             <div class="question">Don't have an account yet? <a href="/register">Register here</a></div>            <button class="submitbtn" type="submit">Submit</button>
             <p class="error submit-error"></p>
             <p class="success register-success"></p>
         </div>
-
     </form>
 </section>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,35 +18,35 @@
                 <div class="form-group"><input class="inputbox" type="text" name="name"
                         placeholder="ex. Delvin Setiamin"></div>
                 @error('name')
-                    <span class="d-block fs-6 text-danger my-2">{{ $message }}</span>
+                    <span class="error text-danger">{{ $message }}</span>
                 @enderror
 
                 <div class="inputtitle">Username</div>
                 <div class="form-group"><input class="inputbox" type="text" name="username" placeholder="ex. @delvin88">
                 </div>
                 @error('username')
-                    <span class="d-block fs-6 text-danger my-2">{{ $message }}</span>
+                    <span class="error text-danger">{{ $message }}</span>
                 @enderror
 
                 <div class="inputtitle">Email</div>
                 <div class="form-group"><input class="inputbox" type="email" name="email"
                         placeholder="example@gmail.com"></div>
                 @error('email')
-                    <span class="d-block fs-6 text-danger my-2">{{ $message }}</span>
+                    <span class="error text-danger">{{ $message }}</span>
                 @enderror
 
                 <div class="inputtitle">Password</div>
                 <div class="form-group"><input class="inputbox" type="password" name="password"
                         placeholder="more than 8 characters"></div>
                 @error('password')
-                    <span class="d-block fs-6 text-danger my-2">{{ $message }}</span>
+                    <span class="error text-danger">{{ $message }}</span>
                 @enderror
 
                 <div class="inputtitle">Confirm Password</div>
                 <div class="form-group"><input class="inputbox" type="password" name="password_confirmation"
                         placeholder="more than 8 characters"></div>
                 @error('password_confirmation')
-                    <span class="d-block fs-6 text-danger my-2">{{ $message }}</span>
+                    <span class="error text-danger">{{ $message }}</span>
                 @enderror
 
                 <div class="question">Have an account? <a href="/login">Login here</a></div>
