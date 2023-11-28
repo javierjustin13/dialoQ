a04c66c5beaf982e6e710d71d6676009b9d95133
Kezia Revalina Tan <120441245+sparkel124@users.noreply.github.com>
2023-11-28 16:07:51 +0700

    revised register login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,38 +6,33 @@
 
 @section('title', 'Login')
 
-@section('content')
-    <div class="form-login">
-        <div class="container-md-offset-3 row-md-6">
-            @include('widgets.success-message')
-            <form class="form-horizontal" action="{{ route('login') }}" method="POST">
-                @csrf
-                <div class="heading">
-                    <div class="login">dialoQ</div>
-                </div>
-                <div class="login-req">Email : </div>
-                <div class="form-group">
-                    <input type="email" class="form-control" name="email" placeholder="example@gmail.com">
-                    {{-- <i class="fa fa-user"></i> --}}
-                </div>
-                @error('email')
-                    <span class="d-block fs-6 text-danger my-2">{{ $message }}</span>
-                @enderror
+@section('login')
+<section class="login">
+    <div class="loginimg"><img src="/image/dialoQ-register.png" alt="Image"></div>
+    <form name="registerform" action="{{ route('register') }}" method="POST">
+        @csrf
+        <div class="container">
+            <div class="logintitle">Login</div>
 
-                <div class="login-req">Password :</div>
-                <div class="form-group">
-                    <input type="password" class="form-control" name="password" placeholder="Enter your password">
-                    {{-- <i class="fa fa-lock"></i> --}}
-                    <a href="#" class="fa fa-question-circle"></a>
-                </div>
-                @error('password')
-                    <span class="d-block fs-6 text-danger my-2">{{ $message }}</span>
-                @enderror
-                <div class="submit-btn">
-                    <button type="submit" class="button" onclick="disable(this)">SUBMIT</button>
-                </div>
-                <div class="question">Don't have an account yet? <a href="/register">Register here</a></div>
-            </form>
+            <div class="inputtitle">Email</div>
+            <div class="form-group"><input class="inputbox" type="email" name="email"
+                    placeholder="example@gmail.com"></div>
+            @error('email')
+                <span class="d-block fs-6 text-danger my-2">{{ $message }}</span>
+            @enderror
+
+            <div class="inputtitle">Password</div>
+            <div class="form-group"><input class="inputbox" type="password" name="password"
+                    placeholder="more than 8 characters"></div>
+            @error('password')
+                <span class="d-block fs-6 text-danger my-2">{{ $message }}</span>
+            @enderror
+
+            <div class="question">Don't have an account yet? <a href="/register">Register here</a></div>            <button class="submitbtn" type="submit">Submit</button>
+            <p class="error submit-error"></p>
+            <p class="success register-success"></p>
         </div>
-    </div>
+
+    </form>
+</section>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,8 +16,6 @@
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <link rel="stylesheet" href="{{ asset('css/layouts/nav.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/layouts/responsive.css') }}">
-
     @stack('head')
 </head>
 
