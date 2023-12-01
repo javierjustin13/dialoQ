2b06835594b990280a89edba4d77ebbbfbfff69d
Javier Justin <jav.justin2004.jj@gmail.com>
2023-12-01 13:17:32 +0700

    refactored users.show.blade

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -53,4 +53,22 @@ class UserController extends Controller
     {
         return $this->show(auth()->user());
     }
+
+    public function follow(User $user)
+    {
+        $follower = auth()->user();
+
+        $follower->followings()->attach($user);
+
+        return redirect()->back()->with('success', "You've started following $user->username!");
+    }
+
+    public function unfollow(User $user)
+    {
+        $follower = auth()->user();
+
+        $follower->followings()->detach($user);
+
+        return redirect()->back()->with('success', "You've unfollowed $user->username!");
+    }
 }
