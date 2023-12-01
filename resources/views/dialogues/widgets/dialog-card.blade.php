b5c85d358a08b78fac379bdda005a75322af4d5d
Javier Justin <jav.justin2004.jj@gmail.com>
2023-12-01 13:56:33 +0700

    dialog-card background color

--- resources/views/dialogues/widgets/dialog-card.blade.php
+++ resources/views/dialogues/widgets/dialog-card.blade.php
@@ -1,4 +1,4 @@
-<div wire:key="{{ $dialog->id }}" class="p-3 border rounded-4 mb-3" id="postcard">
+<div wire:key="{{ $dialog->id }}" class="p-3 border rounded-4 mb-3" id="postcard" style="background-color: #241B35">
     @include('dialogues.widgets.dialog-card-header')
 
     @include('dialogues.widgets.dialog-card-body')
