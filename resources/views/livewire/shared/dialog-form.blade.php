aaba03679ba0caf20308678a8354c88aa7f39e10
Javier Justin <jav.justin2004.jj@gmail.com>
2023-12-02 18:25:24 +0700

    fixed error share dialog when not logged in

--- resources/views/livewire/shared/dialog-form.blade.php
+++ resources/views/livewire/shared/dialog-form.blade.php
@@ -16,8 +16,14 @@
                 <span class="d-block fs-6 text-danger my-2">{{ $message }}</span>
             @enderror
         </div>
-        <button wire.loading.attr="disabled " wire:click.prevent="storeDialog" class="btn btn-dark my-2 px-4"
-            type="submit" style="background-color: #6C35DE">Share</button>
-        <div wire:loading.delay class="ms-3 spinner-grow spinner-grow-sm text-white" role="status"></div>
+        @auth
+            <button wire.loading.attr="disabled " wire:click.prevent="storeDialog" class="btn btn-dark my-2 px-4"
+                type="submit" style="background-color: #6C35DE">Share</button>
+            <div wire:loading.delay class="ms-3 spinner-grow spinner-grow-sm text-white" role="status"></div>
+        @endauth
+        @guest
+            <a href="{{ route('login') }}" class="btn btn-dark my-2 px-4" type="submit"
+                style="background-color: #6C35DE">Share</a>
+        @endguest
     </form>
 </div>
