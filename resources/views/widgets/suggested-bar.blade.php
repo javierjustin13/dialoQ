63c90e485fa64acdbdbde882e8392371bffc154d
Javier Justin <jav.justin2004.jj@gmail.com>
2023-12-03 01:26:12 +0700

    suggested people functionality

--- resources/views/widgets/suggested-bar.blade.php
+++ resources/views/widgets/suggested-bar.blade.php
@@ -5,24 +5,37 @@
 <div class="card mb-3 border" style="max-width: 18rem;background-color: #342A45">
     <div class="card-body ">
         <div class="card mb-3 border" style="background-color: #241B35" id="suggestedtext">
-            <p>
-                SUGGESTED FOR YOU
-            </p>
+            <p>PEOPLE YOU MAY KNOW</p>
         </div>
-        <div class="suggestedpeople pb-3">
-            <div class="suggestedname">
-                <img src="../../image/profile-image.jpeg" alt="">
-                <p class="pl-2">Javier Justin</p>
+        @auth
+            @php
+                if(Auth::user()->followings->count() > 0){
+                    $randomPickedFollowings = Auth::user()->followings->random();
+                    if($randomPickedFollowings->followings->count() > 0){
+                        $suggestedPeople = $randomPickedFollowings->followings->random(3)->where('id', '!=', Auth::user()->id)->whereNotIn('id', Auth::user()->followings->pluck('id'));
+                    }
+                    else{
+                        $suggestedPeople = App\Models\User::where('id', '!=', Auth::user()->id)->whereNotIn('id', Auth::user()->followings->pluck('id'))->inRandomOrder()->take(3)->get();
+                    }
+                }
+                else{
+                    $suggestedPeople = App\Models\User::where('id', '!=', Auth::user()->id)->whereNotIn('id', Auth::user()->followings->pluck('id'))->inRandomOrder()->take(3)->get();
+                }
+            @endphp
+        @endauth
+        @guest
+            @php
+                $suggestedPeople = App\Models\User::all()->random(3);
+            @endphp
+        @endguest
+        @foreach ($suggestedPeople as $suggested)
+            <div class="suggestedpeople mt-4">
+                <div class="suggestedname">
+                    <img src="{{ $suggested->getImageURL() }}" alt="">
+                    <p class="pl-2">{{ $suggested->username }}</p>
+                </div>
+                <livewire:follow.follow-list-button :user="$suggested" :key="$suggested->id" />
             </div>
-            <img src="../../image/add-user.png" alt="">
-        </div>
-        <div class="suggestedpeople">
-            <div class="suggestedname">
-                <img src="../../image/profile-image.jpeg" alt="">
-                <p class="pl-2">Kezia revalina</p>
-            </div>
-            <img src="../../image/add-user.png" alt="">
-        </div>
-
+        @endforeach
     </div>
 </div>
