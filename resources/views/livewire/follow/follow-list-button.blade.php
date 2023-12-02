c1db43af3d35de72151294714199b8d670a19b7d
Javier Justin <jav.justin2004.jj@gmail.com>
2023-12-02 18:28:59 +0700

    fixed not logged in error when visiting profile

--- resources/views/livewire/follow/follow-list-button.blade.php
+++ resources/views/livewire/follow/follow-list-button.blade.php
@@ -1,19 +1,21 @@
 <div class="me-3">
     <div>
-        @if (Auth::user()->id != $user->id)
-            @if (Auth::user()->follows($user))
-                <button wire:click="toggleFollow()" wire:loading.attr="disabled" type="submit"
-                    class="btn followingbutton d-flex justify-content-center align-items-center" style="font-size: 14px">
-                    <div wire:loading.remove class="follow-text" style="text-align: center">Following</div>
-                    <div wire:loading.delay class="spinner-grow spinner-grow-sm text-white" role="status"></div>
-                </button>
-            @else
-                <button wire:click="toggleFollow()" wire:loading.attr="disabled" type="submit"
-                    class="btn followbutton d-flex align-items-center align-items-center" style="font-size: 14px">
-                    <div wire:loading.remove class="follow-text" style="text-align: center">Follow</div>
-                    <div wire:loading.delay class="spinner-grow spinner-grow-sm text-white" role="status"></div>
-                </button>
+        @auth
+            @if (Auth::user()->id != $user->id)
+                @if (Auth::user()->follows($user))
+                    <button wire:click="toggleFollow()" wire:loading.attr="disabled" type="submit"
+                        class="btn followingbutton d-flex justify-content-center align-items-center" style="font-size: 14px">
+                        <div wire:loading.remove class="follow-text" style="text-align: center">Following</div>
+                        <div wire:loading.delay class="spinner-grow spinner-grow-sm text-white" role="status"></div>
+                    </button>
+                @else
+                    <button wire:click="toggleFollow()" wire:loading.attr="disabled" type="submit"
+                        class="btn followbutton d-flex align-items-center align-items-center" style="font-size: 14px">
+                        <div wire:loading.remove class="follow-text" style="text-align: center">Follow</div>
+                        <div wire:loading.delay class="spinner-grow spinner-grow-sm text-white" role="status"></div>
+                    </button>
+                @endif
             @endif
-        @endif
+        @endauth
     </div>
 </div>
