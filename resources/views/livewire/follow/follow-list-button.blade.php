250f38ee4e493dc70c20359b59e1805a966efe93
Kezia Revalina Tan <120441245+sparkel124@users.noreply.github.com>
2023-12-03 15:06:25 +0700

    fixed follow button in suggested card and dialog card

--- resources/views/livewire/follow/follow-list-button.blade.php
+++ resources/views/livewire/follow/follow-list-button.blade.php
@@ -4,15 +4,16 @@
             @if (Auth::user()->id != $user->id)
                 @if (Auth::user()->follows($user))
                     <button wire:click="toggleFollow()" wire:loading.attr="disabled" type="submit"
-                        class="btn followingbutton d-flex justify-content-center align-items-center" style="font-size: 14px">
+                        class="btn followingbutton d-flex justify-content-center align-items-center"
+                        style="font-size: 10px; width: 70px; height: 25px; background-color: #FCEAFC; color: #6C35DE; display: flex; justify-content: center;">
                         <div wire:loading.remove class="follow-text" style="text-align: center">Following</div>
                         <div wire:loading.delay class="spinner-grow spinner-grow-sm text-white" role="status"></div>
                     </button>
                 @else
                     <button wire:click="toggleFollow()" wire:loading.attr="disabled" type="submit"
-                        class="btn followbutton d-flex align-items-center align-items-center" style="font-size: 14px">
-                        <div wire:loading.remove class="follow-text" style="text-align: center">Follow</div>
-                        <div wire:loading.delay class="spinner-grow spinner-grow-sm text-white" role="status"></div>
+                        class="btn followbutton d-flex align-items-center align-items-center" style="font-size: 10px;  width: 70px; height: 25px; background-color: #6C35DE; color:#FCEAFC; display: flex; justify-content: center;">
+                        <div wire:loading.remove class="follow-text" style="text-align: center;">Follow</div>
+                        <div wire:loading.delay class="spinner-grow spinner-grow-sm text-white" style="background-color: white" role="status"></div>
                     </button>
                 @endif
             @endif
--- resources/views/livewire/follow/remove-follower-list-button.blade.php
+++ resources/views/livewire/follow/remove-follower-list-button.blade.php
@@ -4,13 +4,13 @@
             @if (Auth::user()->id != $user->id)
                 @if ($user->follows(Auth::user()))
                     <button wire:click="toggleRemove" wire:loading.attr="disabled" type="submit"
-                        class="btn followingbutton d-flex justify-content-center align-items-center" style="font-size: 14px">
+                        class="btn followingbutton d-flex justify-content-center align-items-center" style="font-size: 14px;  width: 85px; height: 35px; background-color: #4D425F; color:#FCEAFC; display: flex; justify-content: center;">
                         <div wire:loading.remove class="follow-text" style="text-align: center">Remove</div>
                         <div wire:loading.delay class="spinner-grow spinner-grow-sm text-white" role="status"></div>
                     </button>
                 @else
                     <button disabled
-                        class="btn followbutton d-flex align-items-center align-items-center" style="font-size: 14px">
+                        class="btn followbutton d-flex align-items-center align-items-center" style="font-size: 14px;  width: 85px; height: 35px; background-color: #FCEAFC; color:#4D425F; display: flex; justify-content: center;">
                         <div wire:loading.remove class="follow-text" style="text-align: center">Removed</div>
                     </button>
                 @endif
