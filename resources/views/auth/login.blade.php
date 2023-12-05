8cb52663465d04ed627081c32987757ee57e241c
Interblues88 <delvin.setiamin.08@gmail.com>
2023-12-05 13:24:05 +0700

    making login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,79 @@
 @extends('layouts.app')
 
 @push('head')
-    <link rel="stylesheet" href="{{ asset('css/auth/login.css') }}">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{ asset('fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{ asset('fonts/iconic/css/material-design-iconic-font.min.css') }}">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{ asset('vendor/animate/animate.css') }}">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{ asset('vendor/css-hamburgers/hamburgers.min.css') }}">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{ asset('vendor/animsition/css/animsition.min.css') }}">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{ asset('vendor/select2/select2.min.css') }}">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{ asset('vendor/daterangepicker/daterangepicker.css') }}">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{ asset('css/auth/login/util.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('css/auth/login/main.css')}}">
+<!--===============================================================================================-->
 @endpush
 
 @section('title', 'Login')
 
 {{-- @section('success')
-    @include('widgets.success-message')
+@include('widgets.success-message')
 @endsection --}}
 
 @section('login')
-    <section class="login">
-        <div class="loginimg"><img src="/image/dialoQ-register.png" alt="Image"></div>
-        <form name="loginform" action="{{ route('login') }}" method="POST">
-            @csrf
-            <div class="container">
+<div class="limiter">
+    <div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+        <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+            <form class="login100-form validate-form" action="{{ route('login') }}" method="POST">
+                @csrf
+                <span class="login100-form-title p-b-49">
+                    Login
+                </span>
 
-                <div class="logintitle">
-                    <div class="success-message">
-                        @include('widgets.success-message')
-                    </div>
-                    <h1>Login</h1>
+                <div class="wrap-input100 validate-input">
+                    <span class="label-input100">Email</span>
+                    <input class="input100" type="text" name="email" placeholder="Type your email">
+                    <span class="focus-input100" data-symbol="&#xf206;"></span>
                 </div>
-                <div class="inputtitle">Email</div>
-                <div class="form-group"><input class="inputbox" type="email" name="email"
-                        placeholder="example@gmail.com"></div>
                 @error('email')
                     <span class="error text-danger">{{ $message }}</span>
                 @enderror
 
-                <div class="inputtitle">Password</div>
-                <div class="form-group"><input class="inputbox" type="password" name="password"
-                        placeholder="more than 8 characters"></div>
+                <div class="wrap-input100 validate-input m-t-23">
+                    <span class="label-input100">Password</span>
+                    <input class="input100" type="password" name="password" placeholder="Type your password">
+                    <span class="focus-input100" data-symbol="&#xf190;"></span>
+                </div>
                 @error('password')
                     <span class="error text-danger">{{ $message }}</span>
                 @enderror
 
-                <div class="question">Don't have an account yet? <a href="/register">Register here</a></div> <button
-                    class="submitbtn" type="submit">Submit</button>
-                <p class="error submit-error"></p>
-                <p class="success register-success"></p>
-            </div>
-        </form>
-    </section>
+                <div class="container-login100-form-btn p-t-50">
+                    <div class="wrap-login100-form-btn">
+                        <div class="login100-form-bgbtn"></div>
+                        <button class="login100-form-btn" type="submit">
+                            Login
+                        </button>
+                    </div>
+                </div>
+
+                <div class="flex-col-c p-t-50">
+                    <a href="{{ route('register')}}" class="txt2">
+                        Sign Up
+                    </a>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
