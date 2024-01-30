80f6fe1effb60cbd687ac6db96d224821965dcd6
Kezia Revalina Tan <120441245+sparkel124@users.noreply.github.com>
2024-01-31 04:59:41 +0700

    fixed comment time responsive

--- resources/views/widgets/comment.blade.php
+++ resources/views/widgets/comment.blade.php
@@ -1,5 +1,5 @@
 @push('head')
-<link rel="stylesheet" href="{{ asset('css/widgets/comment.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/widgets/comment.css') }}">
 @endpush
 
 
@@ -11,28 +11,30 @@
 
     {{-- End Modal --}}
 
-<div class="card-header mb-3 comment-item ">
-    <div class="media flex-wrap w-100 align-items-center"> <img src="{{ $comment->user->getImageURL() }}"
-            class="d-block ui-w-40 rounded-circle" alt="">
-        <div class="media-body ml-3"> <a href="{{ route('users.show', $comment->user->id) }}">{{ $comment->user->username }}</a>
+    <div class="card-header mb-3 comment-item ">
+        <div class="media flex-wrap w-100 align-items-center"> <img src="{{ $comment->user->getImageURL() }}"
+                class="d-block ui-w-40 rounded-circle" alt="">
+            <div class="media-body ml-3"> <a
+                    href="{{ route('users.show', $comment->user->id) }}">{{ $comment->user->username }}</a>
+            </div>
+            @auth()
+                @can('delete', $comment)
+                    <button class="ms-1 btn btn-danger btn-sm" data-bs-toggle="modal"
+                        data-bs-target="#CommentModal{{ $comment->id }}"> X </button>
+                @endcan
+            @endauth
         </div>
-        @auth()
-        @can('delete', $comment)
-        <button class="ms-1 btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#CommentModal{{ $comment->id }}"> X </button>
-        @endcan
-        @endauth
-    </div>
-    <div class="commentfooter">
-        <p class=" ms-5 pl-2" id="textcomment">{{ $comment->content }}</p>
-        <div class="text-muted small ml-3" id="timing">
-            <i class="bi bi-clock me-1" style="color: #FCEAFC"></i>
-            <span style="color: #FCEAFC">
-                {{ $comment->created_at->diffForHumans() }}
-            </span>
+        <div class="commentfooter">
+            <p class=" ms-5 pl-2" id="textcomment">{{ $comment->content }}</p>
+            <div class="time-box" id="timing">
+                <i class="bi bi-clock" style="color: #FCEAFC"></i>
+                <span class="time-text" style="color: #FCEAFC">
+                    {{ $comment->created_at->diffForHumans() }}
+                </span>
+            </div>
         </div>
     </div>
-</div>
-<hr style="color: #FCEAFC">
+    <hr style="color: #FCEAFC">
 @empty
-<p class="text-center mt-4" style="color:#FCEAFC ">No Comments Found.</p>
+    <p class="text-center mt-4" style="color:#FCEAFC ">No Comments Found.</p>
 @endforelse
