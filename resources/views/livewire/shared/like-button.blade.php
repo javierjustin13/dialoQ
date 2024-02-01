56192b95a4b65c9ab72ebe09dbe98c63b04a3835
Kezia Revalina Tan <120441245+sparkel124@users.noreply.github.com>
2024-02-01 21:56:50 +0700

    changed like button color into red

--- resources/views/livewire/shared/like-button.blade.php
+++ resources/views/livewire/shared/like-button.blade.php
@@ -2,21 +2,21 @@
     @auth()
         @if (Auth::user()->hasLiked($dialog))
             <button wire:loading.attr="disabled" wire:click="toggleLike()" type="submit">
-                <i wire:loading.delay.remove class="bi bi-heart-fill" style="color: #67696D"></i>
+                <i wire:loading.delay.remove class="bi bi-heart-fill" style="color: red"></i>
             </button>
-            <div wire:loading.delay class="spinner-grow spinner-grow-sm text-white" role="status"></div>
+            <div wire:loading.delay class="spinner-grow spinner-grow-sm" role="status" style="color: red"></div>
             <span style="color: #67696D">{{ $count }}</span>
         @else
             <button wire:loading.attr="disabled" wire:click="toggleLike()" type="submit">
-                <i wire:loading.delay.remove class="bi bi-heart" style="color: #67696D"></i>
+                <i wire:loading.delay.remove class="bi bi-heart" style="color: red"></i>
             </button>
-            <div wire:loading.delay class="spinner-grow spinner-grow-sm text-white" role="status"></div>
+            <div wire:loading.delay class="spinner-grow spinner-grow-sm" role="status" style="color: red"></div>
             <span style="color: #67696D">{{ $count }}</span>
         @endif
     @endauth
 
     @guest
         <a href="{{ route('login') }}"><i class="bi bi-heart"></i></a>
-        <span style="color: #67696D">{{ $count }}</span>
+        <span style="color: red">{{ $count }}</span>
     @endguest
 </div>
--- resources/views/widgets/view-card.blade.php
+++ resources/views/widgets/view-card.blade.php
@@ -9,54 +9,49 @@
 
     {{-- End Modal --}}
 
-
-<div class="p-3 pt-0 border rounded-4" id="midpane">
-    <div class="p-3 border rounded-4 mb-3" id="postcard">
-        <div class="card-header">
-            <div class="media w-100 align-items-center"> <img src="{{ $dialog->user->getImageURL() }}"
-                    class="d-block ui-w-40 rounded-circle" alt="">
-                <div class="media-body ml-3"> <a href="{{ route('users.show', $dialog->user->id) }}"
-                        data-abc="true">{{ $dialog->user->username }}</a>
-                </div>
-                @include('widgets.follow-button')
-                <div class="text-muted small ml-3 d-flex align-items-center">
-                    @auth()
-                        @can('update', $dialog)
-                            <a class="mx-2" href="{{ route('dialogues.edit', $dialog->id) }}"> Edit </a>
-                            <button class="ms-1 btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#DialogModal{{ $dialog->id }}"> X </button>
-                        @endcan
-                    @endauth
-                </div>
+<div class="view-card p-3 border rounded-4 mb-3" style="background-color: white" id="postcard">
+    <div class="card-header">
+        <div class="media w-100 align-items-center"> <img src="{{ $dialog->user->getImageURL() }}"
+                class="d-block ui-w-40 rounded-circle" alt="">
+            <div class="media-body ml-3"> <a href="{{ route('users.show', $dialog->user->id) }}"
+                    data-abc="true">{{ $dialog->user->username }}</a>
             </div>
-        </div>
-        <div class="card-body pt-3">
-            <h4>{{ $dialog->title }}</h4>
-            <p>{{ $dialog->content }}</p>
-        </div>
-        <br>
-        <div class="card-footer align-items-center px-0 pt-0 pb-3">
-            <div class="d-flex justify-content-between">
-                <div class="text-muted d-flex">
-                    <livewire:shared.like-button :dialog="$dialog" :key="$dialog->id"/>
-                    <span class="ms-3">
-                        <a class="bi bi-chat-dots" href="{{ route('dialogues.show', $dialog->id) }}"></a>
-                        <span class="count-comment">{{ $dialog->comments->count() }}</span>
-                    </span>
-                </div>
-                <span class="text-muted">
-                    <i class="bi bi-clock me-1" style="color:#050505"></i>
-                    <span class="duration">
-                        {{ $dialog->created_at->diffForHumans() }}
-                    </span>
-                </span>
+            @include('widgets.follow-button')
+            <div class="text-muted small ml-3 d-flex align-items-center">
+                @auth()
+                    @can('update', $dialog)
+                        <a class="mx-2" href="{{ route('dialogues.edit', $dialog->id) }}"> Edit </a>
+                        <button class="ms-1 btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#DialogModal{{ $dialog->id }}"> X </button>
+                    @endcan
+                @endauth
             </div>
-            <br>
-            @include('widgets.comment-form')
-            <hr style="color: #FCEAFC;border-width:2px">
-            @include('widgets.comment')
-
         </div>
     </div>
+    <div class="card-body pt-3">
+        <h4>{{ $dialog->title }}</h4>
+        <p>{{ $dialog->content }}</p>
+    </div>
+    <br>
+    <div class="card-footer align-items-center px-0 pt-0 pb-3">
+        <div class="d-flex justify-content-between">
+            <div class="text-muted d-flex">
+                <livewire:shared.like-button :dialog="$dialog" :key="$dialog->id"/>
+                <span class="ms-3">
+                    <a class="bi bi-chat-dots" href="{{ route('dialogues.show', $dialog->id) }}"></a>
+                    <span class="count-comment">{{ $dialog->comments->count() }}</span>
+                </span>
+            </div>
+            <span class="text-muted">
+                <i class="bi bi-clock me-1" style="color:#050505"></i>
+                <span class="duration">
+                    {{ $dialog->created_at->diffForHumans() }}
+                </span>
+            </span>
+        </div>
+        <br>
+        @include('widgets.comment-form')
+        <hr style="color: #545454;border-width:2px">
+        @include('widgets.comment')
 
-
+    </div>
 </div>
