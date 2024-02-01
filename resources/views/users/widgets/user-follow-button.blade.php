74304a456ef9b9be29f13c6655f492055c924d1a
Kezia Revalina Tan <120441245+sparkel124@users.noreply.github.com>
2024-02-02 03:59:10 +0700

    changed color follow or unfollow user in user profile card

--- resources/views/users/widgets/user-follow-button.blade.php
+++ resources/views/users/widgets/user-follow-button.blade.php
@@ -6,7 +6,7 @@
                     @csrf
                     <button type="submit"
                         class="btn btn-light d-flex justify-content-center align-items-center"
-                        style="font-size: 14px">
+                        style="font-size: 14px;  background-color: #050505;">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                             fill="currentColor" class="bi bi-person-fill-check" viewBox="0 0 16 16" style="color:white;">
                             <path
@@ -14,7 +14,7 @@
                             <path
                                 d="M2 13c0 1 1 1 1 1h5.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.544-3.393C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4" />
                         </svg>
-                        <div class="follow-text ms-1">Following</div>
+                        <div class="follow-text ms-1" style="color: white;" >Following</div>
                     </button>
                 </form>
             @else
@@ -22,7 +22,7 @@
                     @csrf
                     <button type="submit"
                         class="btn btn-dark d-flex align-items-center align-items-center"
-                        style="font-size: 14px" style="color:white;">
+                        style="font-size: 14px background-color: #039FFC;" style="color:white;">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                             fill="currentColor" class="bi bi-person-fill-add" viewBox="0 0 16 16" style="color:white;">
                             <path
