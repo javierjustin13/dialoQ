77b97404600c141cd85648edcd10fa34bcab0736
Kezia Revalina Tan <120441245+sparkel124@users.noreply.github.com>
2024-02-01 21:04:34 +0700

    changed hamburger color in navbar

--- resources/views/livewire/nav/nav-login-register.blade.php
+++ resources/views/livewire/nav/nav-login-register.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <a class="navbar-brand ms-5" href="{{ route('home') }}">dialoQ</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" style="border: 1px solid #FCEAFC">
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" style="border: 1px solid #050505">
             <span class="navbar-toggler-icon">
                 <div class="line"></div>
                 <div class="line"></div>
