60cf615f917a80a28dea598467328285d469743e
Kezia Revalina Tan <120441245+sparkel124@users.noreply.github.com>
2024-02-02 09:02:47 +0700

    fixed dialog form

--- resources/views/livewire/shared/dialog-form.blade.php
+++ resources/views/livewire/shared/dialog-form.blade.php
@@ -2,7 +2,7 @@
     <link rel="stylesheet" href="{{ asset('css/widgets/dialog-form.css') }}"> {{-- ini buat nyambungin home.css ke home blade nya --}}
 @endpush
 
-<div class="p-3 border rounded-3" data-aos="fade-right" data-aos-delay="200" data-aos-duration="1000">
+<div class="p-3 border rounded-3">
     <h4>Share your dialog...</h4>
     <form>
         <input wire:model="title" class="form-control" name = "title" type="text" placeholder="Dialog Title">
@@ -17,9 +17,9 @@
             @enderror
         </div>
         @auth
-            <button wire.loading.attr="disabled " wire:click.prevent="storeDialog" wire:loading.delay.remove class="btn btn-dark my-2 px-4 border-0"
-                type="submit" style="background-color: #039FFC">Share</button>
-            <div wire:loading.delay class="ms-3 spinner-grow spinner-grow-sm text-white" role="status"></div>
+            <button wire.loading.attr="disabled" wire:click.prevent="storeDialog" wire:loading.delay.remove
+                class="btn btn-dark my-2 px-4 border-0" type="submit" style="background-color: #039FFC">Share</button>
+            <div wire:loading.delay class="ms-3 spinner-grow spinner-grow-sm text-primary" role="status"></div>
         @endauth
         @guest
             <a wire:navigate href="{{ route('login') }}" class="btn my-2 px-4" type="submit"
