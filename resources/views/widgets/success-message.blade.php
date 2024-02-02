708ee4143860f4c56062f5fbb9af51c7552bb408
Kezia Revalina Tan <120441245+sparkel124@users.noreply.github.com>
2024-02-02 07:57:08 +0700

    changed success message color

--- resources/views/widgets/success-message.blade.php
+++ resources/views/widgets/success-message.blade.php
@@ -1,5 +1,5 @@
 @if (session('success'))
-    <div class="alert alert-dismissible fade show" style="background-color: #B8FFBF; color: #050505;box-shadow: 0px 0px 6px 0px rgba(222,224,227,1);
+    <div class="alert alert-dismissible fade show" style="background-color: white; color: #050505;box-shadow: 0px 0px 6px 0px rgba(222,224,227,1);
     " role="alert"  >
         {{ session('success') }}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
