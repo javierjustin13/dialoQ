de567c334a6c9100a503472a7b64d9b11a6ac25f
Kezia Revalina Tan <120441245+sparkel124@users.noreply.github.com>
2024-02-02 08:53:23 +0700

    fixed position delete button on dialog card (show user profile page)

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.app')
 
 @push('head')
-<link rel="stylesheet" href="{{ asset('css/users/show.css') }}">
-<link rel="stylesheet" href="{{ asset('css/widgets/dialog-card.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/users/show.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/widgets/dialog-card.css') }}">
 @endpush
 
 @auth
     @if ($user->id == auth()->user()->id)
         @section('title', 'My Profile')
     @else
-        @section('title', $user->username )
-    @endif
+    @section('title', $user->username)
+@endif
 @endauth
 
 @guest
-    @section('title', $user->username)
+@section('title', $user->username)
 @endguest
 
 
@@ -25,23 +25,22 @@
         @if ($dialogues->count() == 0)
             <h1 style="text-align: center; color:#FCEAFC">There are no dialogues</h1>
         @else
-        <div class="col">
-            @foreach ($dialogues as $dialog)
-                @if ($loop->index % 2 == 0)
-                    @include('dialogues.widgets.dialog-card')
-                @endif
-            @endforeach
-        </div>
+            <div class="col">
+                @foreach ($dialogues as $dialog)
+                    @if ($loop->index % 2 == 0)
+                        @include('dialogues.widgets.dialog-card')
+                    @endif
+                @endforeach
+            </div>
 
-        <div class="col" id="coloumn">
-            @foreach ($dialogues as $dialog)
-                @if ($loop->index % 2 == 1)
-                    @include('dialogues.widgets.dialog-card')
-                @endif
-            @endforeach
-        </div>
+            <div class="col" id="coloumn">
+                @foreach ($dialogues as $dialog)
+                    @if ($loop->index % 2 == 1)
+                        @include('dialogues.widgets.dialog-card')
+                    @endif
+                @endforeach
+            </div>
         @endif
     </div>
 </div>
 @endsection
-
